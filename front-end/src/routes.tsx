72941db3e2e32095bd46289429e7bf8cc9ee1083
Geovane Roberto Rigonato <geovane.rigonato.gil@gmail.com>
2024-05-15 19:55:17 -0300

    feat Create Routes

--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,13 +1,22 @@
 import {Routes, Route} from "react-router-dom"
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-
+import Login from "./screens/Login/Login";
+import Home from "./screens/Home/Home";
+import Payment from "./screens/Payment/Payment";
+import Deposit from "./screens/Desposit/Deposit";
+import OutputGraph from "./screens/OutputGraph/OutputGraph";
+import InputGraph from "./screens/InputGraph/InputGraph";
+import History from "./screens/History/History";
 
 function MainRoutes() {
     return (
         <Routes>
             <Route path="/" element={ <Login/>} />
             <Route path="/home" element={<Home/>} />
+            <Route path="/payment" element={<Payment/>} />
+            <Route path="/deposit" element={<Deposit/>} />
+            <Route path="/output-graph" element={<OutputGraph/>} />
+            <Route path="/input-graph" element={<InputGraph/>} />
+            <Route path="/history" element={<History/>} />
         </Routes>
     )
 }
