89455d18ccde551f76010bca10ddbf37039e085a
Geovane Roberto Rigonato <geovane.rigonato.gil@gmail.com>
2024-05-16 20:35:49 -0300

    refactor deletando alguns arquivos

--- back-end/src/modules/wallets/entities/wallet.entity.ts
+++
@@ -1,7 +0,0 @@
-export default class WalletEntity {
-    balance: number;
-
-    constructor(balance?: number) {
-        this.balance = balance;
-    }
-}
--- movements/movements.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MovementsService } from './movements.service';
-import { MovementsController } from './movements.controller';
-
-@Module({
-  controllers: [MovementsController],
-  providers: [MovementsService],
-})
-export class MovementsModule {}
