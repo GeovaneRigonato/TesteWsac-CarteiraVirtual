ca35b75ce2a5da9231c44d28a92277548e8b7732
Geovane Roberto Rigonato <geovane.rigonato.gil@gmail.com>
2024-05-16 20:36:46 -0300

    feat add Transaction.tsx

---
+++ front-end/src/screens/History/Transaction/Transaction.tsx
@@ -0,0 +1,26 @@
+import PaymentIcon from "../../../assets/Payment";
+import "./Transaction.css";
+
+interface TransactionProps {
+    type: string;
+    value: string;
+    date: string;
+    category: string;
+}
+
+function Transaction( data: TransactionProps) {
+  return (
+    <div className="container">
+      <div className="container-transaction">
+        <h3>{data.type}</h3>
+        <div className="content-transaction">
+          <span>{data.category}</span>
+          <span>{data.value}</span>
+        </div>
+      </div>
+      <span>{data.date}</span>
+    </div>
+  );
+}
+
+export default Transaction;
