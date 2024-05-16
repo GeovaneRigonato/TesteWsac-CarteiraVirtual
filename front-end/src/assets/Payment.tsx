47982f82e275f4c1a973abcc2f6bb2ed47fdc2f7
Geovane Roberto Rigonato <geovane.rigonato.gil@gmail.com>
2024-05-16 20:37:39 -0300

    add Payment.tsx Icon

---
+++ front-end/src/assets/Payment.tsx
@@ -0,0 +1,19 @@
+function PaymentIcon(props) {
+    return (
+      <svg
+        width={24}
+        height={16}
+        viewBox="0 0 24 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path
+          d="M7 0C4.8 0 3 1.8 3 4s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm0 4c-3.9 0-7 1.8-7 4v2h11v-2H2c0-.6 1.8-2 5-2 1.8 0 3.2.5 4 1v-2.2c-1.1-.5-2.5-.8-4-.8zM22 0h-7c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zm-6 14h-1V2h1v12zm6 0h-4V2h4v12z"
+          fill="#ffffff"
+        />
+      </svg>
+    )
+  }
+  
+  export default PaymentIcon
